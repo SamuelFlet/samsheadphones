99400b2e45b991d04f1fb9abb51da12d2a141420
Samuel Fletcher <49329336+SamuelFlet@users.noreply.github.com>
2022-03-17 14:51:00 -0400

    Initial Commit

---
+++ src/pages/App.js
@@ -0,0 +1,13 @@
+import "../App.css"
+import axios from "axios";
+import List from "../components/List.jsx"
+
+function App() {
+  return (
+  <div className="App">
+    Headphone List
+    <List />
+    </div>);
+}
+
+export default App;
