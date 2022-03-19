bdba3dc05f6a9f84e5e6c96a71e73895339f1eca
Samuel Fletcher <49329336+SamuelFlet@users.noreply.github.com>
2022-03-19 13:33:04 -0400

    Now displays username of review and name of headphone on page

--- src/pages/headphone.jsx
+++ src/pages/headphone.jsx
@@ -3,21 +3,31 @@ import React, { useEffect, useState } from "react";
 
 export default function Invoice() {
   const [data, setData] = useState({});
-
+  const [hdata, sethData] = useState({});
   let params = useParams();
 
   const axios = require("axios");
+  const fetchData = () => {
+    const reviewAPI = "https://backendhead.herokuapp.com/reviews/";
+    const headphoneAPI =
+      "https://backendhead.herokuapp.com/headphones/" + params.headphoneId;
 
-  useEffect(() => {
-    axios
-      .get("https://backendhead.herokuapp.com/reviews/")
-      .then(function (response) {
-        setData(response.data);
+    const getReviews = axios.get(reviewAPI);
+    const getHeadphone = axios.get(headphoneAPI);
+
+    axios.all([getReviews, getHeadphone]).then(
+      axios.spread((...allData) => {
+        const allreview = allData[0].data;
+        const headphones = allData[1].data;
+
+        setData(allreview);
+        sethData(headphones);
       })
-      .catch(function (error) {
-        console.log(error);
-      });
-  });
+    );
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   var rows = [];
   for (var i = 0; i < data.length; i++) {
@@ -25,9 +35,22 @@ export default function Invoice() {
     // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
     let num = data[i].headphone.toString();
     if (num === params.headphoneId) {
-      rows.push(<div>{data[i].username}: {data[i].review}</div>);
+      rows.push(
+        <div>
+          {data[i].username}: {data[i].review}
+        </div>
+      );
     }
   }
 
-  return <div>{rows}</div>;
+  return (
+  <div>
+    {hdata.name}
+    <br></br>
+    {hdata.description}
+    <br></br>
+    <br></br>
+    {rows}
+  </div>);
+  
 }
